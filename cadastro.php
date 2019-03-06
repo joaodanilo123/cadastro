8505323e8de3fd38e5912acd3465ec95152455b6
João Danilo Zucolotto <joao@pop-os.localdomain>
2019-03-06 18:05:51 -0300

    change w3-css link

--- cadastro.php
+++ cadastro.php
@@ -11,8 +11,8 @@ include 'includes/register.php';
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="style/resetstyle.css">
-    <link rel="stylesheet" href="../w3.css">
+    <link rel="stylesheet" href="style/resetcss.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="style/ajustes.css">
     <title>Cadastro</title>
 </head>
