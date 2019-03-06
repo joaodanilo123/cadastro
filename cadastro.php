1737c3c2ff94ef1b657781d548262addade89a6f
João Danilo Zucolotto <joao@pop-os.localdomain>
2019-03-06 09:26:24 -0300

    identação do html

--- cadastro.php
+++ cadastro.php
@@ -9,24 +9,35 @@ include 'includes/register.php';
 <!DOCTYPE html>
 <html>
 <head>
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8" />
     <link rel="stylesheet" href="style/main.css">
-    <title>cadastro</title>
+    <title>Cadastro</title>
 </head>
 <body> 
     <div class="form">
-    <h2>SIGN UP</h2>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-    <span>Username: </span> <br> <input class='ipt' type="text" name='user'>       <br>
-    <span>Email:</span> <br> <input class='ipt' type="text" name='email'>      <br>
-    <span>Password:</span> <br> <input class='ipt' type="password" name='senha1'> <br>
-    Confirm password:<br>    <input class='ipt' type="password" name='senha2'> <br>
-    <input type="submit" value="SIGN UP">
+        <h2>SIGN UP</h2>
+        <form 
+        action="<?php echo htmlentities($_SERVER["PHP_SELF"]);?>" method="POST">
+        
+        <span>Username:</span><br> 
+            <input class='ipt' type="text" name='user'><br>
+        
+        <span>Email:</span><br> 
+            <input class='ipt' type="text" name='email'>      <br>
+        
+        <span>Password:</span> <br> 
+            <input class='ipt' type="password" name='senha1'> <br>
+        
+        <span>Confirm password:</span><br>    
+            <input class='ipt' type="password" name='senha2'> <br>
+        
+        <input type="submit" value="SIGN UP">
     </form>
-    <span class="error"><?php 
-    echo $user_error . '<br>'.$passwd_error . '<br>'.$email_error . '<br>';
+    
+    <span class="error">
+    <?php 
+        echo $user_error . '<br>'.$passwd_error . '<br>'.$email_error . '<br>';
     ?>
     </span>
     </div>
